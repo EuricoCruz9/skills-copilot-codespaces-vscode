d1d21815c45a8f4a99c12fa46141b302c94e2172
EuricoCruz9 <esouza@palo-it.com>
2025-01-26 16:16:50 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,14 @@
+function skillsMember() {
+  return (
+    <div className="skills-member">
+      <h3>Skills</h3>
+      <ul>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>Node.js</li>
+        <li>Python</li>
+        <li>Java</li>
+      </ul>
+    </div>
+  );
+}
